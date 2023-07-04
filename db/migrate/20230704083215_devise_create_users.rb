5e5761542c88e16b59f0ad18d76b775015e34452
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-04 09:40:47 +0100

    Add full name to devise migration

--- db/migrate/20230704083215_devise_create_users.rb
+++ db/migrate/20230704083215_devise_create_users.rb
@@ -3,6 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      ## Add full name column
+      t.string :full_name, null: false, default: ""
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
