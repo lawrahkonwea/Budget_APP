b1327b4629ea0cd1a452a985876647c26b7323ab
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-04 10:26:17 +0100

    Create transfers and groups models and their migration

--- db/migrate/20230704085706_create_transfers.rb
+++ db/migrate/20230704085706_create_transfers.rb
@@ -1,6 +1,9 @@
 class CreateTransfers < ActiveRecord::Migration[7.0]
   def change
     create_table :transfers do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :author, null: false, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_083215) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_085744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "transfers", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_transfers_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name", default: "", null: false
@@ -31,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_083215) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "transfers", "users", column: "author_id"
 end
