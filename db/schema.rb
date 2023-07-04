6b382982d612ba5baed6a1c9aa326e2094c7dbd8
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-04 10:33:15 +0100

    Create migration for join_tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_085744) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_093053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_085744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_transfers", id: false, force: :cascade do |t|
+    t.bigint "transfer_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id", "transfer_id"], name: "index_groups_transfers_on_group_id_and_transfer_id"
+    t.index ["transfer_id", "group_id"], name: "index_groups_transfers_on_transfer_id_and_group_id"
   end
 
   create_table "transfers", force: :cascade do |t|
