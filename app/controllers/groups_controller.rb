7db606235ae94153c6ca5e034b179dc5903ae465
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-05 09:43:13 +0100

    create methods for category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.created_by_current_user(current_user)
+  end
+
+  def new 
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+    if @group.save
+      redirect_to groups_path, notice: 'Category was successfully created'
+    else
+      render :new
+    end 
+  end
+
+ private
+
+ def group_params
+  params.require(:group).permit(:name, :icon)
+ end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,8 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 250 }
   validates :icon, presence: true, length: { maximum: 250 }
+
+  def self.created_by_current_user(current_user)
+    where(user_id: current_user.id).order(created_at: :desc)
+  end
 end
