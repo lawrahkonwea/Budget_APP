87ea326e028ac53e15a88a3956640c9aee46d284
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-08 20:45:06 +0100

    implement changes

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -18,18 +18,9 @@ class TransfersController < ApplicationController
     @groups_id = params[:group_ids]
 
     @groups_id.each do |id|
-      unless id.blank?
-        group = Group.find_by(id:)
-        @transfer.groups << group unless group.nil?
-      end
+      group = Group.find(id) unless id == ''
+      @transfer.groups.push(group) unless group.nil?
     end
-
-    # @groups_id.each do |id|
-    #   group = Group.find(id) unless id == ''
-    #   @transfer.groups.push(group) unless group.nil?
-    # end
-
-    return :new unless @transfer.groups.any?
 
     if @transfer.save
       redirect_to group_transfers_path(@transfer.groups.first.id), notice: 'Transfer added successfully'
