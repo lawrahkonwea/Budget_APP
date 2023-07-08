4fe8cca3f6268c920a4bba18fa19bf26805372b7
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-08 14:30:03 +0100

    Fix errors

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 
-  RSpec.describe Transfer, type: :model do
-    let(:first_user) { User.create(full_name: 'Tejiri', email: 'bimbo@gmail.com', password: 'qqqqqqqq') }
-    let!(:first_group) { Group.create(name: 'Makeup Kit', icon: 'https://w7.pngwing.com/pngs/542/1010/png-transparent-cosmetics-makeup-brush-lipstick-makeup-brush-miscellaneous-pencil-people-thumbnail.png', user_id: first_user.id) }
-    let(:transfer) { Transfer.new(name: 'Blush', amount: 3.0, author_id: first_user.id) }
-  
-    context 'validation tests' do
-      it 'should not be valid without a name' do
-        transfer.name = nil
-        expect(transfer).to_not be_valid
-      end
-  
-      it 'should not be valid without an amount' do
-        transfer.amount = nil
-        expect(transfer).to_not be_valid
-      end
-  
-      it 'amount must be a number and nothing else' do
-        transfer.amount = 'makeup'
-        expect(transfer).to_not be_valid
-      end
-  
-      it 'must have a valid author ID' do
-        transfer.author_id = nil
-        expect(transfer).to_not be_valid
-      end
-    end
-end
-  
+RSpec.describe Transfer, type: :model do
+  let(:first_user) { User.create(full_name: 'Tejiri', email: 'bimbo@gmail.com', password: 'qqqqqqqq') }
+  let!(:first_group) { Group.create(name: 'Makeup Kit', icon: 'https://w7.pngwing.com/pngs/542/1010/png-transparent-cosmetics-makeup-brush-lipstick-makeup-brush-miscellaneous-pencil-people-thumbnail.png', user_id: first_user.id) }
+  let(:transfer) { Transfer.new(name: 'Blush', amount: 3.0, author_id: first_user.id) }
 
+  context 'validation tests' do
+    it 'should not be valid without a name' do
+      transfer.name = nil
+      expect(transfer).to_not be_valid
+    end
+
+    it 'should not be valid without an amount' do
+      transfer.amount = nil
+      expect(transfer).to_not be_valid
+    end
+
+    it 'amount must be a number and nothing else' do
+      transfer.amount = 'makeup'
+      expect(transfer).to_not be_valid
+    end
+
+    it 'must have a valid author ID' do
+      transfer.author_id = nil
+      expect(transfer).to_not be_valid
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "signed_in successfully" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'signed_in successfully' do
       get new_user_session_path
       expect(response).to have_http_status(200)
     end
 
-    it "signed_up successfully" do
+    it 'signed_up successfully' do
       get new_user_registration_path
       expect(response).to have_http_status(200)
     end
